6fc2f49106c0e1c819690f9be26c45205ac10951
Eric Leite <ericxleite@gmail.com>
2020-09-29 01:17:29 -0700

    Updates

---
+++ precache-manifest.453d4ff0dc84468b25a44c640245c6b7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44442e3f758ac103886f8ec4c0c98fae",
+    "url": "/pinterest-dashboard/index.html"
+  },
+  {
+    "revision": "13153981e125a90a92e8",
+    "url": "/pinterest-dashboard/static/css/main.e41d175b.chunk.css"
+  },
+  {
+    "revision": "090a09c566185d688ab1",
+    "url": "/pinterest-dashboard/static/js/2.705efefa.chunk.js"
+  },
+  {
+    "revision": "13153981e125a90a92e8",
+    "url": "/pinterest-dashboard/static/js/main.b360c247.chunk.js"
+  },
+  {
+    "revision": "ead0302a549ed5917145",
+    "url": "/pinterest-dashboard/static/js/runtime-main.edd90828.js"
+  }
+]);
